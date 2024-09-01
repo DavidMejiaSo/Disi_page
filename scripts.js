4eaf0912ebbff138f58c99e6bb78441dc700b700
DavidMejiaSo <davidmejiasoto97@gmail.com>
2024-08-31 19:20:12 -0500

    UPDATE: Background load fixed

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// Archivo: script.js
+window.addEventListener('load', function() {
+    document.body.classList.remove('loading');
+    document.body.style.backgroundImage = "url('img/fondo.webp')";
+    document.body.classList.add('loaded');
+});
